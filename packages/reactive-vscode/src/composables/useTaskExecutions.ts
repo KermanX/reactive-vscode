7fd5d89230b0595c88dc9cdf501457a57b896462
_Kerman <kermanx@qq.com>
2024-05-18 18:53:47 +0800

    feat: add `useTaskExecutions`

---
+++ packages/reactive-vscode/src/composables/useTaskExecutions.ts
@@ -0,0 +1,17 @@
+import { computed, shallowRef } from '@vue/runtime-core'
+import { tasks } from 'vscode'
+import { createSingletonComposable } from '../utils'
+import { useDisposable } from './useDisposable'
+
+export const useTaskExecutions = createSingletonComposable(() => {
+  const taskExecutions = shallowRef(tasks.taskExecutions)
+
+  function update() {
+    taskExecutions.value = tasks.taskExecutions
+  }
+
+  useDisposable(tasks.onDidStartTask(update))
+  useDisposable(tasks.onDidEndTask(update))
+
+  return computed(() => taskExecutions.value)
+})
