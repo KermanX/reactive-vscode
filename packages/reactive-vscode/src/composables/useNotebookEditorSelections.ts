1d7e40aefbde87346e7aec34dadcc2fdbb46ecb8
_Kerman <kermanx@qq.com>
2024-05-19 22:51:45 +0800

    feat: add `useNotebookEditorSelections`

---
+++ packages/reactive-vscode/src/composables/useNotebookEditorSelections.ts
@@ -0,0 +1,23 @@
+import { computed, shallowRef } from '@vue/runtime-core'
+import type { NotebookEditor } from 'vscode'
+import { window } from 'vscode'
+import { useDisposable } from './useDisposable'
+
+export function useNotebookEditorSelections(notebookEditor: NotebookEditor) {
+  const selections = shallowRef(notebookEditor.selections)
+
+  useDisposable(window.onDidChangeNotebookEditorSelection((ev) => {
+    if (ev.notebookEditor === notebookEditor)
+      selections.value = ev.selections
+  }))
+
+  return computed({
+    get() {
+      return selections.value
+    },
+    set(newSelections) {
+      selections.value = newSelections
+      notebookEditor.selections = newSelections
+    },
+  })
+}
