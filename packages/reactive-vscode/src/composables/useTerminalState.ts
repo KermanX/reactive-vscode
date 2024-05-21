63b68e2ceab584b022a371b22ae42590cc540570
_Kerman <kermanx@qq.com>
2024-05-21 23:11:33 +0800

    feat: add `useTerminalState`

---
+++ packages/reactive-vscode/src/composables/useTerminalState.ts
@@ -0,0 +1,19 @@
+import { computed, shallowRef } from '@vue/runtime-core'
+import type { Terminal } from 'vscode'
+import { window } from 'vscode'
+import { createKeyedComposable } from '../utils'
+import { useDisposable } from './useDisposable'
+
+export const useTerminalState = createKeyedComposable(
+  (terminal: Terminal) => {
+    const state = shallowRef(terminal.state)
+
+    useDisposable(window.onDidChangeTerminalState((ev) => {
+      if (ev === terminal)
+        state.value = terminal.state
+    }))
+
+    return computed(() => state.value)
+  },
+  terminal => terminal,
+)
