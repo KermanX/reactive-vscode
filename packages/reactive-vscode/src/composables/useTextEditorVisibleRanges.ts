3b1993f1fe7c5b0793e678d984c83d58cec9a290
_Kerman <kermanx@qq.com>
2024-05-21 23:07:30 +0800

    feat: add `useTextEditorVisibleRanges`

---
+++ packages/reactive-vscode/src/composables/useTextEditorVisibleRanges.ts
@@ -0,0 +1,19 @@
+import { computed, shallowRef } from '@vue/runtime-core'
+import type { TextEditor } from 'vscode'
+import { window } from 'vscode'
+import { createKeyedComposable } from '../utils'
+import { useDisposable } from './useDisposable'
+
+export const useTextEditorVisibleRanges = createKeyedComposable(
+  (textEditor: TextEditor) => {
+    const ranges = shallowRef(textEditor.visibleRanges)
+
+    useDisposable(window.onDidChangeTextEditorVisibleRanges((ev) => {
+      if (ev.textEditor === textEditor)
+        ranges.value = ev.visibleRanges
+    }))
+
+    return computed(() => ranges.value)
+  },
+  textEditor => textEditor,
+)
