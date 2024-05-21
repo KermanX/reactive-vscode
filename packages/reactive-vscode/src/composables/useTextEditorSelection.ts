8387a14423811c5d90b879081044a19d13ca4d63
_Kerman <kermanx@qq.com>
2024-05-21 23:13:50 +0800

    feat: add `useTextEditorSelection`

---
+++ packages/reactive-vscode/src/composables/useTextEditorSelection.ts
@@ -0,0 +1,27 @@
+import { computed, shallowRef } from '@vue/runtime-core'
+import type { TextEditor } from 'vscode'
+import { window } from 'vscode'
+import { createKeyedComposable } from '../utils'
+import { useDisposable } from './useDisposable'
+
+export const useTextEditorSelection = createKeyedComposable(
+  (textEditor: TextEditor) => {
+    const selection = shallowRef(textEditor.selection)
+
+    useDisposable(window.onDidChangeTextEditorSelection((ev) => {
+      if (ev.textEditor === textEditor)
+        selection.value = ev.selections[0]
+    }))
+
+    return computed({
+      get() {
+        return selection.value
+      },
+      set(newSelection) {
+        selection.value = newSelection
+        textEditor.selection = newSelection
+      },
+    })
+  },
+  textEditor => textEditor,
+)
