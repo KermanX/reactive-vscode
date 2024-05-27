00031b2b29530f6b25c309a57fbab903e786dab0
_Kerman <kermanx@qq.com>
2024-05-27 13:30:37 +0800

    feat: add `useFileUri`

---
+++ packages/reactive-vscode/src/composables/useFileUri.ts
@@ -0,0 +1,11 @@
+import type { MaybeRefOrGetter } from '@vue/runtime-core'
+import { toValue } from '@vue/runtime-core'
+import { Uri } from 'vscode'
+import { useUri } from './useUri'
+
+/**
+ * @reactive `Uri.file`
+ */
+export function useFileUri(path: MaybeRefOrGetter<string>) {
+  return useUri(() => Uri.file(toValue(path)))
+}
