6e157457e698245b0ba982cd0bf926573e192ec5
_Kerman <kermanx@qq.com>
2024-06-03 23:51:57 +0800

    ci: fix release ci

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,6 +1,4 @@
 import { $, fs } from 'zx'
 
 await fs.copyFile('README.md', 'packages/core/README.md')
-await $`pnpm -C packages/core exec npm publish --access public --no-git-checks`
-await $`pnpm -C packages/metadata exec npm publish --access public --no-git-checks`
-await $`pnpm -C packages/creator exec npm publish --access public --no-git-checks`
+await $`pnpm -r --filter "./packages/**" publish --access public --no-git-checks`
