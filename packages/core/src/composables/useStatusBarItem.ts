be7ffbd73b2354fbcd8bc1d6fdd46321f02181ed
_Kerman <kermanx@qq.com>
2024-06-03 17:06:34 +0800

    feat: add `useStatusBarItem`

---
+++ packages/core/src/composables/useStatusBarItem.ts
@@ -0,0 +1,45 @@
+import type { MaybeRefOrGetter } from '@vue/runtime-core'
+import { toValue, watchEffect } from '@vue/runtime-core'
+import type { StatusBarAlignment, StatusBarItem } from 'vscode'
+import { window } from 'vscode'
+import { useDisposable } from './useDisposable'
+
+export interface UseStatusBarItemOptions {
+  id?: string
+  alignment?: StatusBarAlignment
+  priority?: number
+  name?: MaybeRefOrGetter<StatusBarItem['name']>
+  text?: MaybeRefOrGetter<StatusBarItem['text']>
+  tooltip?: MaybeRefOrGetter<StatusBarItem['tooltip']>
+  color?: MaybeRefOrGetter<StatusBarItem['color']>
+  backgroundColor?: MaybeRefOrGetter<StatusBarItem['backgroundColor']>
+  command?: MaybeRefOrGetter<StatusBarItem['command']>
+  accessibilityInformation?: MaybeRefOrGetter<StatusBarItem['accessibilityInformation']>
+}
+
+/**
+ * @reactive `window.createStatusBarItem`
+ */
+export function useStatusBarItem(options: UseStatusBarItemOptions): StatusBarItem {
+  const item = useDisposable(options.id
+    ? window.createStatusBarItem(options.id, options.alignment, options.priority)
+    : window.createStatusBarItem(options.alignment, options.priority))
+
+  function reactivelySet(key: string) {
+    const value = (options as any)[key]
+    if (value != null)
+      watchEffect(() => (item as any)[key] = toValue(value))
+  }
+
+  [
+    'name',
+    'text',
+    'tooltip',
+    'color',
+    'backgroundColor',
+    'command',
+    'accessibilityInformation',
+  ].forEach(reactivelySet)
+
+  return item
+}
