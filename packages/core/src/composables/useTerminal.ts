64410812970556b834ea1b114ed481745def3f91
_Kerman <kermanx@qq.com>
2024-06-14 21:06:00 +0800

    feat: add `useTerminal`

---
+++ packages/core/src/composables/useTerminal.ts
@@ -0,0 +1,41 @@
+import type { ComputedRef } from '@reactive-vscode/reactivity'
+import type { ExtensionTerminalOptions, Terminal, TerminalOptions, TerminalState } from 'vscode'
+import { window } from 'vscode'
+import { useDisposable } from './useDisposable'
+import { useTerminalState } from './useTerminalState'
+
+interface UseTerminalReturn extends Omit<Terminal, 'state' | 'dispose'> {
+  terminal: Terminal
+  state: ComputedRef<TerminalState>
+}
+
+/**
+ * @category terminal
+ * @reactive `window.createTerminal()`
+ */
+export function useTerminal(name?: string, shellPath?: string, shellArgs?: readonly string[] | string): UseTerminalReturn
+export function useTerminal(options: TerminalOptions): UseTerminalReturn
+export function useTerminal(options: ExtensionTerminalOptions): UseTerminalReturn
+export function useTerminal(...args: any[]): UseTerminalReturn {
+  const terminal = useDisposable(window.createTerminal(...args))
+
+  return {
+    terminal,
+    get name() {
+      return terminal.name
+    },
+    get processId() {
+      return terminal.processId
+    },
+    get creationOptions() {
+      return terminal.creationOptions
+    },
+    get exitStatus() {
+      return terminal.exitStatus
+    },
+    sendText: terminal.sendText.bind(terminal),
+    show: terminal.show.bind(terminal),
+    hide: terminal.hide.bind(terminal),
+    state: useTerminalState(terminal),
+  }
+}
