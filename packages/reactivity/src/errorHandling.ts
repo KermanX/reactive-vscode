cc5926fbd121b1505ff52efef2a43a3d44eff3fb
_Kerman <kermanx@qq.com>
2024-06-24 12:10:46 +0800

    chore: remove unused error codes

--- packages/reactivity/src/errorHandling.ts
+++ packages/reactivity/src/errorHandling.ts
@@ -5,43 +5,21 @@ import { LifecycleHooks } from './enums'
 // contexts where user provided function may be executed, in addition to
 // lifecycle hooks.
 export enum ErrorCodes {
-  SETUP_FUNCTION,
-  RENDER_FUNCTION,
   WATCH_GETTER,
   WATCH_CALLBACK,
   WATCH_CLEANUP,
-  NATIVE_EVENT_HANDLER,
-  COMPONENT_EVENT_HANDLER,
-  VNODE_HOOK,
-  DIRECTIVE_HOOK,
-  TRANSITION_HOOK,
   APP_ERROR_HANDLER,
-  APP_WARN_HANDLER,
-  FUNCTION_REF,
-  ASYNC_COMPONENT_LOADER,
   SCHEDULER,
 }
 
 export const ErrorTypeStrings: Record<LifecycleHooks | ErrorCodes, string> = {
   [LifecycleHooks.ACTIVATED]: 'activated hook',
   [LifecycleHooks.DEACTIVATED]: 'deactivated hook',
-  [ErrorCodes.SETUP_FUNCTION]: 'setup function',
-  [ErrorCodes.RENDER_FUNCTION]: 'render function',
   [ErrorCodes.WATCH_GETTER]: 'watcher getter',
   [ErrorCodes.WATCH_CALLBACK]: 'watcher callback',
   [ErrorCodes.WATCH_CLEANUP]: 'watcher cleanup function',
-  [ErrorCodes.NATIVE_EVENT_HANDLER]: 'native event handler',
-  [ErrorCodes.COMPONENT_EVENT_HANDLER]: 'component event handler',
-  [ErrorCodes.VNODE_HOOK]: 'vnode hook',
-  [ErrorCodes.DIRECTIVE_HOOK]: 'directive hook',
-  [ErrorCodes.TRANSITION_HOOK]: 'transition hook',
   [ErrorCodes.APP_ERROR_HANDLER]: 'app errorHandler',
-  [ErrorCodes.APP_WARN_HANDLER]: 'app warnHandler',
-  [ErrorCodes.FUNCTION_REF]: 'ref function',
-  [ErrorCodes.ASYNC_COMPONENT_LOADER]: 'async component loader',
-  [ErrorCodes.SCHEDULER]:
-    'scheduler flush. This is likely a Vue internals bug. ' +
-    'Please open an issue at https://github.com/vuejs/core .',
+  [ErrorCodes.SCHEDULER]: 'scheduler flush',
 }
 
 export type ErrorTypes = LifecycleHooks | ErrorCodes
