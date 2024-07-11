dff1302b1aa83c9fb5b9bba6155facfe498c841f
Anthony Fu <github@antfu.me>
2024-07-12 04:19:42 +0800

    feat: support `createConfigRef` `defineConfigsWithDefaults` and ref write back

--- packages/core/src/utils/defineConfigs.ts
+++ packages/core/src/utils/defineConfigs.ts
@@ -1,6 +1,6 @@
 import type { ShallowRef } from '@reactive-vscode/reactivity'
-import { shallowRef } from '@reactive-vscode/reactivity'
-import type { ConfigurationScope, ConfigurationTarget } from 'vscode'
+import { computed, shallowRef } from '@reactive-vscode/reactivity'
+import type { ConfigurationScope, ConfigurationTarget, WorkspaceConfiguration } from 'vscode'
 import { workspace } from 'vscode'
 import { useDisposable } from '../composables'
 import { onActivate } from './onActivate'
@@ -40,6 +40,49 @@ type ParseConfigTypeOptions<C extends ConfigTypeOptions> = {
   [K in keyof C]: ConfigRef<ParseConfigType<C[K]>>
 }
 
+function _createConfigRef<T>(workspaceConfig: WorkspaceConfiguration, key: string, value: T): ConfigRef<T> {
+  const data = shallowRef(value)
+
+  async function update(value: T, configurationTarget?: ConfigurationTarget | boolean | null, overrideInLanguage?: boolean) {
+    data.value = value
+    await workspaceConfig.update(key, value, configurationTarget, overrideInLanguage)
+  }
+
+  const ref = computed({
+    get() {
+      return data.value
+    },
+    set(v) {
+      data.value = v
+      update(v)
+    },
+  }) as unknown as ConfigRef<T>
+
+  ref.update = update
+
+  // @ts-expect-error internal function
+  ref._set = (v: T) => {
+    data.value = v
+  }
+
+  return ref
+}
+
+export function createConfigRef<T>(key: string, value: T): ConfigRef<T> {
+  const ref = _createConfigRef(workspace.getConfiguration(), key, value)
+
+  onActivate(() => {
+    useDisposable(workspace.onDidChangeConfiguration((e) => {
+      if (!e.affectsConfiguration(key))
+        return
+      // @ts-expect-error internal function
+      ref._set(workspace.getConfiguration().get(key))
+    }))
+  })
+
+  return ref
+}
+
 /**
  * Define configurations of an extension. See `vscode::workspace.getConfiguration`.
  *
@@ -48,18 +91,9 @@ type ParseConfigTypeOptions<C extends ConfigTypeOptions> = {
 export function defineConfigs<const C extends ConfigTypeOptions>(section: string, configs: C, scope?: ConfigurationScope | null | undefined): ParseConfigTypeOptions<C> {
   const workspaceConfig = workspace.getConfiguration(section, scope)
 
-  function createConfigRef<T>(key: string, value: T): ConfigRef<T> {
-    const ref = shallowRef(value) as unknown as ConfigRef<T>
-    ref.update = async (value, configurationTarget, overrideInLanguage) => {
-      await workspaceConfig.update(key, value, configurationTarget, overrideInLanguage)
-      ref.value = value
-    }
-    return ref
-  }
-
   const configRefs = Object.fromEntries(
     Object.keys(configs).map((key) => {
-      return [key, createConfigRef(key, workspaceConfig.get(key))]
+      return [key, _createConfigRef(workspaceConfig, key, workspaceConfig.get(key))]
     }),
   ) as ParseConfigTypeOptions<C>
 
@@ -70,7 +104,42 @@ export function defineConfigs<const C extends ConfigTypeOptions>(section: string
       const newWorkspaceConfig = workspace.getConfiguration(section)
       for (const key in configs) {
         if (e.affectsConfiguration(`${section}.${key}`))
-          configRefs[key].value = newWorkspaceConfig.get(key) as any
+          // @ts-expect-error internal function
+          configRefs[key]._set(newWorkspaceConfig.get(key))
+      }
+    }))
+  })
+
+  return configRefs
+}
+
+/**
+ * Define configurations of an extension. See `vscode::workspace.getConfiguration`.
+ *
+ * @category lifecycle
+ */
+export function defineConfigsWithDefaults<const C extends object>(
+  section: string,
+  defaults: C,
+  scope?: ConfigurationScope | null | undefined,
+): { [K in keyof C]: ConfigRef<C[K]> } {
+  const workspaceConfig = workspace.getConfiguration(section, scope)
+
+  const configRefs = Object.fromEntries(
+    Object.entries(defaults).map(([key, value]) => {
+      return [key, _createConfigRef(workspaceConfig, key, value)]
+    }),
+  ) as { [K in keyof C]: ConfigRef<C[K]> }
+
+  onActivate(() => {
+    useDisposable(workspace.onDidChangeConfiguration((e) => {
+      if (!e.affectsConfiguration(section))
+        return
+      const newWorkspaceConfig = workspace.getConfiguration(section)
+      for (const key in defaults) {
+        if (e.affectsConfiguration(`${section}.${key}`))
+          // @ts-expect-error internal function
+          configRefs[key]._set(newWorkspaceConfig.get(key))
       }
     }))
   })
