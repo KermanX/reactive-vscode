e27097172a782ceb10e7b1e71901604f1dc6243d
_Kerman <kermanx@qq.com>
2024-08-15 15:41:38 +0800

    chore: fix typos

--- packages/core/src/composables/useAbsolutePath.ts
+++ packages/core/src/composables/useAbsolutePath.ts
@@ -6,8 +6,8 @@ import { asAbsolutePath } from '../utils/asAbsolutePath'
  * @category utilities
  * @reactive `ExtensionContext.asAbsolutePath`
  */
-export function useAbsolutePath(relativePath: MaybeRefOrGetter<string>, slient: true): ComputedRef<string | undefined>
-export function useAbsolutePath(relativePath: MaybeRefOrGetter<string>, slient?: false): ComputedRef<string>
-export function useAbsolutePath(relativePath: MaybeRefOrGetter<string>, slient = false) {
-  return computed(() => asAbsolutePath(toValue(relativePath), slient))
+export function useAbsolutePath(relativePath: MaybeRefOrGetter<string>, silent: true): ComputedRef<string | undefined>
+export function useAbsolutePath(relativePath: MaybeRefOrGetter<string>, silent?: false): ComputedRef<string>
+export function useAbsolutePath(relativePath: MaybeRefOrGetter<string>, silent = false) {
+  return computed(() => asAbsolutePath(toValue(relativePath), silent))
 }
--- packages/core/src/utils/asAbsolutePath.ts
+++ packages/core/src/utils/asAbsolutePath.ts
@@ -3,11 +3,11 @@ import { extensionContext } from '../utils'
 /**
  * A shorthand for `vscode::ExtensionContext.asAbsolutePath`
  */
-export function asAbsolutePath(relativePath: string, slient?: false): string
-export function asAbsolutePath(relativePath: string, slient?: boolean): string | undefined
-export function asAbsolutePath(relativePath: string, slient = false) {
+export function asAbsolutePath(relativePath: string, silent?: false): string
+export function asAbsolutePath(relativePath: string, silent?: boolean): string | undefined
+export function asAbsolutePath(relativePath: string, silent = false) {
   const extCtx = extensionContext.value
-  if (!extCtx && !slient)
+  if (!extCtx && !silent)
     throw new Error('Cannot get absolute path because the extension is not activated yet')
   return extCtx?.asAbsolutePath(relativePath)
 }
