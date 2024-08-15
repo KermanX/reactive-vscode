d5654291f2acaa95151837f508f909ba16016006
_Kerman <kermanx@qq.com>
2024-08-15 15:36:48 +0800

    fix: remove `useUri`

--- packages/core/src/composables/useFileUri.ts
+++ packages/core/src/composables/useFileUri.ts
@@ -1,12 +1,11 @@
 import type { MaybeRefOrGetter } from '@reactive-vscode/reactivity'
-import { toValue } from '@reactive-vscode/reactivity'
+import { computed, toValue } from '@reactive-vscode/reactivity'
 import { Uri } from 'vscode'
-import { useUri } from './useUri'
 
 /**
  * @reactive `Uri.file`
  * @category utilities
  */
 export function useFileUri(path: MaybeRefOrGetter<string>) {
-  return useUri(() => Uri.file(toValue(path)))
+  return computed(() => Uri.file(toValue(path)))
 }
