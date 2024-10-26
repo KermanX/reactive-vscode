41322e3c9e01ab6aa9590f29bd2092f4423d4f58
_Kerman <kermanx@qq.com>
2024-10-26 22:30:02 +0800

    docs: fix `generated-meta.ts` snippet

--- docs/snippets/generated-meta.ts
+++ docs/snippets/generated-meta.ts
@@ -1,17 +1,19 @@
-export interface ScopedConfigKeyTypeMap {
+export interface NestedScopedConfigs {
   disabled: boolean
-  autoDetection: boolean
-  localesPaths: undefined
-  encoding: string
+  decoration: {
+    enabled: boolean
+    color: string
+    mode: 'text' | 'gutter'
+  }
 }
 
 export const scopedConfigs = {
   scope: 'i18n-ally',
   defaults: {
-    disabled: false,
-    autoDetection: true,
-    localesPaths: undefined,
-    encoding: 'utf-8',
+    'disabled': false,
+    'decoration.enabled': true,
+    'decoration.color': '#ff0000',
+    'decoration.mode': 'text',
     // ...
   },
 }
